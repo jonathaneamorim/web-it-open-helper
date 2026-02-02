5ae63cd0fa3cdfd12a122665afa7a742ba5727e9
jonathaneamorim <jonathaneamorim@gmail.com>
2026-02-02 09:03:52 -0300

    fix: Registry refactoring

--- src/data/registry.ts
+++ src/data/registry.ts
@@ -4,7 +4,8 @@ import gitData from "./programming/git.json";
 import cssData from "./programming/css.json";
 import htmlData from "./programming/html.json";
 import markdownData from "./programming/markdown.json";
-
+import softwareData from "./softwares/desktop_linux.json";
+import soLinuxData from "./so/linux.json";
 
 export interface TopicBlock {
   type: string;
@@ -30,16 +31,19 @@ export interface Level {
 
 export interface PageContent {
   trackTitle: string;
+  category: string; 
   description?: string;
   levels: Level[];
   links?: Array<{ title: string; url: string }>;
 }
 
 export const contentRegistry: Record<string, PageContent> = {
-  "javascript": javascriptData as PageContent,
-  "docker": dockerData as PageContent,
-  "git": gitData as PageContent,
-  "css": cssData as PageContent,
-  "html": htmlData as PageContent,
-  "markdown": markdownData as PageContent,
+  "javascript": javascriptData as unknown as PageContent,
+  "docker": dockerData as unknown as PageContent,
+  "git": gitData as unknown as PageContent,
+  "css": cssData as unknown as PageContent,
+  "html": htmlData as unknown as PageContent,
+  "markdown": markdownData as unknown as PageContent,
+  "linux": soLinuxData as unknown as PageContent,
+  "desktop-linux": softwareData as unknown as PageContent,
 };
