ff3bdc47e23e8212f65c0ffcd8fab39dd98497b1
jonathaneamorim <jonathaneamorim@gmail.com>
2026-02-01 21:19:49 -0300

    fix: Deleting files to change the site structure.

--- src/app/programming/databases/mysql/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Mysql() {
-  return (    
-  <div className="p-8">
-      <h1 className="text-2xl font-bold mb-4">MySQL</h1>
-      <p>Conteúdo sobre MySQL.</p>
-    </div>
-  );
-};      
