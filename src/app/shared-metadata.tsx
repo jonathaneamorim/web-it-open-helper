56c536b7f81fb293e847adea8b6a316266570386
jonathaneamorim <jonathaneamorim@gmail.com>
2026-02-05 20:49:42 -0300

    fix: adjusting metadata

--- src/app/shared-metadata.tsx
+++ src/app/shared-metadata.tsx
@@ -8,15 +8,24 @@ export const siteMetadata: Metadata = {
   description: "Central de conhecimento tech, anotações sobre Programação, SO e Ferramentas.",
   authors: [{ name: "Jonathan Amorim", url: "https://github.com/jonathaneamorim" }],
   keywords: ["Next.js", "React", "Programação", "Anotações", "Linux"],
-  icons: {
-    icon: '/favicon.ico',
-  },
-
   openGraph: {
     title: "IT Open Helper",
-    description: "Sua central de conhecimento tech.",
+    description: "Sua central de conhecimento tech. Aqui você encontra anotações sobre Programação, Sistemas Operacionais e Ferramentas. Tudo organizado para facilitar seu aprendizado e consulta rápida.",
     siteName: "IT Open Helper",
     locale: "pt_BR",
     type: "website",
   },
+  icons: {
+    icon: [
+      { url: '/favicon.ico', sizes: 'any' },
+      { url: '/icon-16x16.png', type: 'image/png', sizes: '16x16' },
+      { url: '/icon-32x32.png', type: 'image/png', sizes: '32x32' },
+      { url: '/android-chrome-192x192.png', type: 'image/png', sizes: '192x192' },
+    ],
+    shortcut: ['/favicon.ico'],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+  },
+  manifest: '/site.webmanifest',
 };
