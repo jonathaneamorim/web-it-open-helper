387511f7b37fb9bc0a2b668ae720e0d6c86a2126
jonathaneamorim <jonathaneamorim@gmail.com>
2026-02-06 21:09:13 -0300

    fix: removing comments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Header } from "@/components/header/header"; // Ajustei para o header novo
+import { Header } from "@/components/header/header";
 import { siteMetadata } from "@/app/shared-metadata";
 
 const geistSans = Geist({
